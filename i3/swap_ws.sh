b00439b1521bc2dc2fcf8f6e7f8cbac3da5ea1d9
Stefan Shklifov <s.shklifov@gmail.com>
2020-04-19 10:41:01 +0300

    Revert "migrating to centos"
    This reverts commit 8b1072cf582ddaa4d1b36f7c0e198e0c6a633434.

--- i3/swap_ws.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-focused=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true) | .name')
-other=$1
-tmp="0"
-
-i3-msg "rename workspace $focused to $tmp"
-i3-msg "rename workspace $other to $focused" &> /dev/null
-i3-msg "rename workspace $tmp to $other"
--- zshenv
+++
@@ -1,3 +0,0 @@
-export KEYTIMEOUT=1
-export EDITOR=/usr/bin/nvim
-#export QT_QPA_PLATFORMTHEME=gtk2
